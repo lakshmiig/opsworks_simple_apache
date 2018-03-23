8df882ebf5570779a0298ca5ef518a6ce1605351
Vijay Bheemineni <bheemineni.vijay@gmail.com>
2018-03-23 16:48:57 -0500

    Implemented Simple Apache.

---
+++ README.md
@@ -0,0 +1,3 @@
+# simple_apache
+
+This cookbook installs httpd24 package and creates simple index.html page.
---
+++ recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook:: simple_apache
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'httpd'
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
